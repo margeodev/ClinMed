ec9c3d72e95eb87e947bf7cf53dc8be00ceb69fc
Marcio <margeodev@gmail.com>
2016-08-22 14:07:34 -0300

    Implementado filtros de busca para médicos usando nome, CRM e especialidade como filtro, método na classe Medicos (repository) monta uma busca dinâmica de acordo so os parâmetros recebidos

--- src/main/java/com/sistema/controller/PesquisaMedicoBean.java
+++ src/main/java/com/sistema/controller/PesquisaMedicoBean.java
@@ -1,41 +1,42 @@
 package com.sistema.controller;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
-import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
 import com.sistema.modelo.Medico;
 import com.sistema.repository.Medicos;
+import com.sistema.repository.filter.MedicoFilter;
 
 @Named("pmb")
+@SessionScoped
 public class PesquisaMedicoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Inject
 	private Medicos medicosRep;
-	private List<Medico> medicos = new ArrayList<>();
-	private Medico medico;
 	
-	@PostConstruct
-	public void init(){
-		medicos = medicosRep.buscarTodos();
-	}
+	private List<Medico> medicos;
+	private MedicoFilter medFilter;
 		
-	public Medico getMedico() {
-		return medico;
+	public PesquisaMedicoBean(){		
+		medFilter = new MedicoFilter();
 	}
-
-	public void setMedico(Medico medico) {
-		this.medico = medico;
+	
+	public void pesquisar(){		
+		medicos = medicosRep.filtrar(medFilter);
 	}
 
 	public List<Medico> getMedicos() {
 		return medicos;
+	}
+
+	public MedicoFilter getMedFilter() {
+		return medFilter;
 	}
 	
 }
--- src/main/java/com/sistema/repository/Medicos.java
+++ src/main/java/com/sistema/repository/Medicos.java
@@ -5,10 +5,17 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
-import javax.persistence.EntityTransaction;
 import javax.persistence.NoResultException;
+import org.apache.commons.lang3.StringUtils;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
 
+import com.sistema.modelo.Especialidade;
 import com.sistema.modelo.Medico;
+import com.sistema.repository.filter.MedicoFilter;
 
 public class Medicos implements Serializable {
 	
@@ -21,10 +28,10 @@ public class Medicos implements Serializable {
 		em.merge(medico);			
 	}
 	
-	@SuppressWarnings("unchecked")
-	public List<Medico> buscarTodos(){
-		return em.createQuery("from Medico").getResultList();
-	}	
+//	@SuppressWarnings("unchecked")
+//	public List<Medico> buscarTodos(){
+//		return em.createQuery("from Medico").getResultList();
+//	}	
 
 	public Medico porCRM(String crm) {
 		try {
@@ -33,6 +40,23 @@ public class Medicos implements Serializable {
 		} catch (NoResultException e) {
 			return null;
 		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Medico> filtrar(MedicoFilter filtro){
+		Session session = em.unwrap(Session.class);
+		Criteria criteria = session.createCriteria(Medico.class).createAlias("especialidade", "e");
+		
+		if (StringUtils.isNotBlank(filtro.getCrm())){
+			criteria.add(Restrictions.eq("crm", filtro.getCrm()));
+		}
+		if (StringUtils.isNotBlank(filtro.getNome())){
+			criteria.add(Restrictions.ilike("nome", filtro.getNome(), MatchMode.ANYWHERE));
+		}
+		if (StringUtils.isNotBlank(filtro.getEspecialidade())){
+			criteria.add(Restrictions.ilike("e.descricao", filtro.getEspecialidade(), MatchMode.ANYWHERE));
+		}
+		return criteria.addOrder(Order.asc("nome")).list();
 	}
 	
 }
